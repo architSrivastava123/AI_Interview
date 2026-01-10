890b2eedabb2357ea2b2f123a79797fb42db058c
architSrivastava123 <architsrivastava159@gmail.com>
2026-01-10 14:40:22 +0530

    Implement active interview page skeleton and state orchestration

---
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -0,0 +1,7 @@
+
+"use client";
+import React from 'react';
+function QuestionsSection() {
+  return <div>Questions Section</div>;
+}
+export default QuestionsSection;
---
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -0,0 +1,7 @@
+
+"use client";
+import React from 'react';
+function RecordAnswerSection() {
+  return <div>Record Answer Section</div>;
+}
+export default RecordAnswerSection;
